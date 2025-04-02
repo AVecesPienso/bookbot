c29a79029fbb92fc6f0b62fb7de53b954195c053
AVecesPienso <a.escarcega.r@outlook.com>
2025-04-02 05:49:34 -0600

    Finished project

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-from stats import count_words
+import sys
+from stats import count_words, count_chars, sort_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+
     num_words = count_words(book_text)
-    print(f"{num_words} words found in the document")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+    print("--------- Character Count -------")
+    char_counts = count_chars(book_text)
+    sorted_chars = sort_chars(char_counts)
+
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        count = char_dict["count"]
+
+        if char.isalpha():
+            print(f"{char}: {count}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,22 @@ def count_words(text):
     words = text.split()
     total_words = len(words)
     return total_words
+
+def count_chars(text):
+    char_dict = {}
+    for char in text:
+        lowercase_char = char.lower()
+        if lowercase_char in char_dict:
+            char_dict[lowercase_char] += 1
+        else:
+            char_dict[lowercase_char] = 1
+    return char_dict
+
+def sort_chars(char_dict):
+    char_list = []
+    for char, count in char_dict.items():
+        char_list.append({"char": char, "count": count})
+    def sort_on(char_dict):
+        return char_dict["count"]
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
